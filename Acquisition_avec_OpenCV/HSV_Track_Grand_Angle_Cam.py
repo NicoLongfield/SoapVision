78d33582b89a7a115efb00dc5e3d3eb7d1fc3d62
NicoLongfield <72104945+NicoLongfield@users.noreply.github.com>
2021-10-20 13:15:31 -0400

    Début du cleaning - Création du fichier function_cam

--- Acquisition_avec_OpenCV/HSV_Track_Grand_Angle_Cam.py
+++ Acquisition_avec_OpenCV/HSV_Track_Grand_Angle_Cam.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import sys
 from csi_camera import CSI_Camera
+from function_cam import *
 from tracker import *
 from OPC_UA import *
 import time
@@ -16,40 +17,17 @@ tracker = EuclideanDistTracker()
 OPCUA = OPCUACommunication()
 
 OPCUA_Pause = sys.argv[1]
-
-
-def nothing(x):
-    pass
-# Simple draw label on an image; in our case, the video frame
-def draw_label(cv_image, label_text, label_position):
-    font_face = cv2.FONT_HERSHEY_SIMPLEX
-    scale = 0.5
-    color = (255,255,255)
-    # You can get the size of the string with cv2.getTextSize here
-    cv2.putText(cv_image, label_text, label_position, font_face, scale, color, 1, cv2.LINE_AA)
-
-def read_camera(csi_camera,display_fps):
-    _ , camera_image=csi_camera.read()
-    if display_fps:
-        draw_label(camera_image, "Frames Displayed (PS): "+str(csi_camera.last_frames_displayed),(10,20))
-        draw_label(camera_image, "Frames Read (PS): "+str(csi_camera.last_frames_read),(10,40))
-    return camera_image
+right_camera = CSI_Camera()
+left_camera = CSI_Camera()
+print(OPCUA_Pause)
 
 DISPLAY_WIDTH=960
 DISPLAY_HEIGHT=720
-
 SENSOR_MODE_720=3
 
 def start_cameras():
-    
-    cv2.namedWindow('Trackbars')
-    cv2.createTrackbar('hueLower', 'Trackbars',91,179,nothing)
-    cv2.createTrackbar('hueUpper', 'Trackbars',106,179,nothing)
-    cv2.createTrackbar('satLow', 'Trackbars',91,255,nothing)
-    cv2.createTrackbar('satHigh', 'Trackbars',255,255,nothing)
-    cv2.createTrackbar('valLow','Trackbars',126,255,nothing)
-    cv2.createTrackbar('valHigh','Trackbars',255,255,nothing)
 
+    init_display()
     left_camera = CSI_Camera()
     left_camera.create_gstreamer_pipeline(
             sensor_id=0,
@@ -73,12 +51,8 @@ def start_cameras():
     )
     right_camera.open(right_camera.gstreamer_pipeline)
     right_camera.start()
-
-    cv2.namedWindow("CSI Cameras", cv2.WINDOW_NORMAL)
-    cv2.namedWindow("HSV-Mask and Track", cv2.WINDOW_NORMAL)
-    cv2.namedWindow("Object Track", cv2.WINDOW_NORMAL)
     if (
-        #not right_camera.video_capture.isOpened()
+        not right_camera.video_capture.isOpened()
     ):
         # Cameras did not open, or no camera attached
 
@@ -90,7 +64,7 @@ def start_cameras():
         left_camera.start_counting_fps()
         while cv2.getWindowProperty("CSI Cameras", 0) >= 0 :
             #left_image = read_camera(left_camera, False)
-            img=read_camera(right_camera,show_fps)
+            img=cv2.VideoCapture(0)
             hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
             hueLow=cv2.getTrackbarPos('hueLower', 'Trackbars')
---
+++ Acquisition_avec_OpenCV/function_cam.py
@@ -0,0 +1,35 @@
+from csi_camera import CSI_Camera
+from tracker import *
+import cv2
+
+
+def nothing(x):
+    pass
+# Simple draw label on an image; in our case, the video frame
+def draw_label(cv_image, label_text, label_position):
+    font_face = cv2.FONT_HERSHEY_SIMPLEX
+    scale = 0.5
+    color = (255,255,255)
+    # You can get the size of the string with cv2.getTextSize here
+    cv2.putText(cv_image, label_text, label_position, font_face, scale, color, 1, cv2.LINE_AA)
+
+def read_camera(csi_camera,display_fps):
+    _ , camera_image=csi_camera.read()
+    if display_fps:
+        draw_label(camera_image, "Frames Displayed (PS): "+str(csi_camera.last_frames_displayed),(10,20))
+        draw_label(camera_image, "Frames Read (PS): "+str(csi_camera.last_frames_read),(10,40))
+    return camera_image
+
+def init_display():
+    cv2.namedWindow('Trackbars')
+    cv2.createTrackbar('hueLower', 'Trackbars',91,179,nothing)
+    cv2.createTrackbar('hueUpper', 'Trackbars',106,179,nothing)
+    cv2.createTrackbar('satLow', 'Trackbars',91,255,nothing)
+    cv2.createTrackbar('satHigh', 'Trackbars',255,255,nothing)
+    cv2.createTrackbar('valLow','Trackbars',126,255,nothing)
+    cv2.createTrackbar('valHigh','Trackbars',255,255,nothing)
+    
+    cv2.namedWindow("CSI Cameras", cv2.WINDOW_NORMAL)
+    cv2.namedWindow("HSV-Mask and Track", cv2.WINDOW_NORMAL)
+    cv2.namedWindow("Object Track", cv2.WINDOW_NORMAL)
+
